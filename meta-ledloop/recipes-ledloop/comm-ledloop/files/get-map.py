5f22ed1f42c563c91f0afc6b5827de31343967de
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2023-12-20 17:48:36 +0100

    added get-map.py to backend python scripts

--- meta-ledloop/recipes-ledloop/comm-ledloop/files/get-map.py
+++ meta-ledloop/recipes-ledloop/comm-ledloop/files/get-map.py
@@ -3,7 +3,7 @@ import json
 import os
 
 APP_PATH = os.path.dirname(os.path.realpath(__file__))
-WORK_MODE_JSON_FILE = os.path.join(APP_PATH, "figures-mode.json")
+WORK_MODE_JSON_FILE = os.path.join(APP_PATH, "led-map.json")
 
 def load_work_json():
     with open(WORK_MODE_JSON_FILE, "r") as f:
